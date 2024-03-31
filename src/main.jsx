f9a6a03a1c8f26abbbabd815ca186323624bf3d3
andriy <evicandrij@gmail.com>
2024-03-31 15:35:44 +0300

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Admin-dashboard">
+    <BrowserRouter basename="/Admin-dashboard-frontend">
     <Provider store={store}>
     <PersistGate  persistor={persistor}>
         <App />
