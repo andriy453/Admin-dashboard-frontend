1a24dad6b6ad8f98f8171f0c180718ddb0c7b7c6
andriy <evicandrij@gmail.com>
2024-03-31 14:41:14 +0300

    fix

--- src/assets/main.jsx
+++ src/assets/main.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import { PersistGate } from "redux-persist/integration/react";
-import { store, persistor } from "./redux/store.js";
+import { store, persistor } from "../redux/store.js";
 import { Provider } from "react-redux";
-import App from './App.jsx'; 
-import '../src/index.css';
+import App from '../App.jsx'; 
+import '../index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
